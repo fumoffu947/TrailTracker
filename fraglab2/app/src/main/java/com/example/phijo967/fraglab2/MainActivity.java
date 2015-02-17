06e5cc645f23bf6107677a69025ee4b401f2909e
Philip Johansson <phijo967@student.liu.se>
2015-02-17 14:17:17 +0100

    lade till fragmentlabben test

---
+++ fraglab2/app/src/main/java/com/example/phijo967/fraglab2/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.phijo967.fraglab2;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends FragmentActivity implements Facebook.OnFragmentInteractionListener,MainFrag.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new Facebook())
+                    .commit();
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new MainFrag())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void switchFrag(String data) {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, new MainFrag()).commit();
+    }
+
+
+    @Override
+    public void goFace() {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, new Facebook()).commit();
+    }
+}
